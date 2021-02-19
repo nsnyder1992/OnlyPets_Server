7e86123ccfa88690017f3b448831fa2b1c0d8b3d
arnettryan <73567190+arnettryan@users.noreply.github.com>
2021-02-18 20:34:24 -0500

    pet-controller

--- controllers/pet-controller.js
+++ controllers/pet-controller.js
@@ -1,17 +1,73 @@
-const router = require("express").Router();
+const express = require("express");
+const router = express.Router();
+const sequelize = require("../db");
+const { route } = require("./user-controller");
+const Pet = sequelize.import("../models/pet.js");
 
-//database
-const Pet = require("../db").import("../models/pet.js");
 
-//validation
-// const validateSession = require("../middleware/validate-session");
 
-////////////////////////////////////////////////
-// PET TEST
-////////////////////////////////////////////////
-router.get("/test", (req, res) => {
-  console.log("test");
-  res.json({ test: true });
+
+router.get("/", (req, res) => {
+  Pet.findAll()
+    .then((pets) => {
+      if (pets.length === 0)
+        return res
+          .status(200)
+          .json({ message: "No pets found!" });
+      res.status(200).json({ pets });
+    })
+    .catch((error) => {
+      res.status(500).json({ error });
+    });
 });
 
+router.post("/create", (req, res) => {
+  // Pet.create({
+  //   name: req.body.pet.name,
+  //   type: req.body.pet.type,
+  //   description: req.body.pet.description,
+  //   moneyToSuscribe: req.body.pet.moneyToSuscribe,
+  //   ownerId: req.body.pet.ownerId
+  // })
+  //   .then((pet) => {
+  //     res.status(200).json(pet);
+  //   })
+  //   .catch((error) => {
+  //     res.status(500).json(error);
+  //   });
+
+  res.json({mesg:"ksjdhfksjdhf"})
+});
+router.delete("/:id", (req, res) => {
+  Pet.destroy({ where: { id: req.params.id, } })     //userId: req.user.id 
+    .then((response) => {
+      res.status(200).json({
+        message: "Successfully deleted pet!",
+        numOfDeleted: response,
+      });
+    })
+    .catch((error) => {
+      res.status(500).json(error);
+    });
+});
+router.put("/:id", (req, res) => {
+  Pet.update(req.body, {
+    where: { id: req.params.id },
+  })
+    .then((response) => {
+      res.status(200).json({ message: "Updated Successfully", response });
+    })
+    .catch((error) => {
+      res.status(500).json(error);
+    });
+});
+
+
 module.exports = router;
+
+
+
+
+
+
+
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,18 +1,61 @@
-const router = require("express").Router();
-
-//database
-const User = require("../db").import("../models/user.js");
-
-//Authentication
+require("dotenv")
+let express = require("express");
+const router =require("express").Router();
+const User=require("../db").import("../models/user.js");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
+let bcrypt = require("bcryptjs");
 
-////////////////////////////////////////////////
-// USER TEST
-////////////////////////////////////////////////
-router.get("/test", (req, res) => {
-  console.log("test");
-  res.json({ test: true });
+
+
+router.post("/create", function(req, res){
+    User.create({
+        email: req.body.user.email,
+        password: bcrypt.hashSync(req.body.user.password, 13)
+    })
+    .then(function createSuccess(user){
+           let token= jwt.sign({id: user.id, email: user.email}, process.env.JWT_SECRET, {expiresIn: 60*60*24});
+          
+           res.json({
+               user: user,
+               message: "User successfully created",
+               sessionToken: token
+           });
+       }
+    )
+
+    .catch(err => res.status(500).json({error: err }))
+});
+
+router.post('/login', function(req, res){
+
+    User.findOne({
+        where: {
+            email: req.body.user.email
+        }
+    })
+
+        .then(function loginSuccess(user){
+          if (user){
+              bcrypt.compare(req.body.user.password, user.password, function (err, matches){
+                  if (matches){
+              
+              let token = jwt.sign({id: user.id, email: user.email}, process.env.JWT_SECRET, {expiresIn: 60*60*24})
+           
+              res.status(200).json({
+                user: user,
+                message: "User successfully created",
+                sessionToken: token
+                
+            })
+        }else{
+            res.status(502).send({error: "Login Failed"});
+        }
+        });
+    }else{
+            res.status(500).json({error: 'User does not exist.'})
+        }
+        })
+        .catch(err => res.status(500).json({error: err}))
 });
 
 module.exports = router;
